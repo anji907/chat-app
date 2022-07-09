403a110ca903f8a19e686c17226a5e40abd45f06
anji907 <anji.sk129@gmail.com>
2022-07-09 17:16:57 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @room = Room.find(@message.room_id)
+
+    if @message.save
+      redirect_to room_messages_path(@message.room_id)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id, room_id: params[:room_id])
   end
 end
